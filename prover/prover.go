4d43ae7498937f67cfcbeafc13cfc873e0dff6a3
rianhughes <rian.hughes@physics.ox.ac.uk>
2024-03-20 12:30:41 +0200

    wip

--- prover/prover.go
+++ prover/prover.go
@@ -7,7 +7,6 @@ package prover
 
 #define FELT_SIZE 32
 
-// Todo: placeholder
 typedef struct BlockInfo {
 	unsigned long long block_number;
 	unsigned long long block_timestamp;
@@ -21,17 +20,30 @@ typedef struct BlockInfo {
 	unsigned char use_blob_data;
 } BlockInfo;
 
-extern void snosRunnerRun();
+extern void snosRunnerRun(BlockInfo* block_info_ptr, uintptr_t readerHandle, char* chain_id);
 
 // Todo: ???
 #cgo vm_debug  LDFLAGS: -L./rust/target/debug   -ljuno_starknet_rs -ldl -lm
 #cgo !vm_debug LDFLAGS: -L./rust/target/release -ljuno_starknet_rs -ldl -lm
 */
 import "C"
-import "github.com/NethermindEth/juno/utils"
+import (
+	"encoding/json"
+	"runtime/cgo"
+	"unsafe"
+
+	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/utils"
+)
+
+type BlockInfo struct {
+	Header                *core.Header
+	BlockHashToBeRevealed *felt.Felt
+}
 
 type Prover interface {
-	snosRunnerRun() // Tod
+	snosRunnerRun(blockInfo *BlockInfo, useBlobData bool, network *utils.Network, state core.StateReader) error // Todo
 }
 
 type prover struct {
@@ -43,8 +55,69 @@ func New(log utils.SimpleLogger) Prover {
 		log: log,
 	}
 }
-func (p *prover) snosRunnerRun() {
-	// Todo: generate relevant data
-	// Todo: call the rust function
+func (p *prover) snosRunnerRun(blockInfo *BlockInfo, useBlobData bool, network *utils.Network, state core.StateReader) error {
+	// Todo: generate relevant data (block data and transactions in json form)
 	// Todo: handle the "returned" data
+	context := &callContext{
+		state: state,
+		log:   p.log,
+	}
+	handle := cgo.NewHandle(context)
+	defer handle.Delete()
+	cBlockInfo := makeCBlockInfo(blockInfo, useBlobData)
+	chainID := C.CString(network.L2ChainID)
+	C.snosRunnerRun(&cBlockInfo, C.uintptr_t(handle), chainID)
+	C.free(unsafe.Pointer(chainID))
+	C.free(unsafe.Pointer(cBlockInfo.version))
+	return nil
+}
+
+type callContext struct {
+	// state that the call is running on
+	state core.StateReader
+	log   utils.SimpleLogger
+	// err field to be possibly populated in case of an error in execution
+	err string
+	// index of the transaction that generated err
+	errTxnIndex int64
+	// response from the executed Cairo function
+	response []*felt.Felt
+	// fee amount taken per transaction during VM execution
+	actualFees      []*felt.Felt
+	traces          []json.RawMessage
+	dataGasConsumed []*felt.Felt
+}
+
+func makeCBlockInfo(blockInfo *BlockInfo, useBlobData bool) C.BlockInfo {
+	var cBlockInfo C.BlockInfo
+
+	cBlockInfo.block_number = C.ulonglong(blockInfo.Header.Number)
+	cBlockInfo.block_timestamp = C.ulonglong(blockInfo.Header.Timestamp)
+	copyFeltIntoCArray(blockInfo.Header.SequencerAddress, &cBlockInfo.sequencer_address[0])
+	copyFeltIntoCArray(blockInfo.Header.GasPrice, &cBlockInfo.gas_price_wei[0])
+	copyFeltIntoCArray(blockInfo.Header.GasPriceSTRK, &cBlockInfo.gas_price_fri[0])
+	cBlockInfo.version = cstring([]byte(blockInfo.Header.ProtocolVersion))
+	copyFeltIntoCArray(blockInfo.BlockHashToBeRevealed, &cBlockInfo.block_hash_to_be_revealed[0])
+	if blockInfo.Header.L1DAMode == core.Blob {
+		copyFeltIntoCArray(blockInfo.Header.L1DataGasPrice.PriceInWei, &cBlockInfo.data_gas_price_wei[0])
+		copyFeltIntoCArray(blockInfo.Header.L1DataGasPrice.PriceInFri, &cBlockInfo.data_gas_price_fri[0])
+		if useBlobData {
+			cBlockInfo.use_blob_data = 1
+		} else {
+			cBlockInfo.use_blob_data = 0
+		}
+	}
+	return cBlockInfo
+}
+
+func copyFeltIntoCArray(f *felt.Felt, cArrPtr *C.uchar) {
+	if f == nil {
+		return
+	}
+
+	feltBytes := f.Bytes()
+	cArr := unsafe.Slice(cArrPtr, len(feltBytes))
+	for index := range feltBytes {
+		cArr[index] = C.uchar(feltBytes[index])
+	}
 }
--- prover/rust/src/lib.rs
+++ prover/rust/src/lib.rs
@@ -6,6 +6,26 @@ use blockifier::state::cached_state::CachedState;
 use blockifier::block_context::BlockContext;
 
 use crate::juno_state_reader::{ptr_to_felt, JunoStateReader};
+use starknet_api::{
+    deprecated_contract_class::EntryPointType,
+    hash::StarkFelt,
+    transaction::Fee,
+};
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct BlockInfo {
+    pub block_number: c_ulonglong,
+    pub block_timestamp: c_ulonglong,
+    pub sequencer_address: [c_uchar; 32],
+    pub gas_price_wei: [c_uchar; 32],
+    pub gas_price_fri: [c_uchar; 32],
+    pub version: *const c_char,
+    pub block_hash_to_be_revealed: [c_uchar; 32],
+    pub data_gas_price_wei: [c_uchar; 32],
+    pub data_gas_price_fri: [c_uchar; 32],
+    pub use_blob_data: c_uchar,
+}
 
 
 #[no_mangle]
@@ -14,7 +34,7 @@ pub extern "C" fn snosRunnerRun(
     reader_handle: usize,
     chain_id: *const c_char,
 ) {
-    
+    let block_info = unsafe { *block_info_ptr };
     let reader = JunoStateReader::new(reader_handle, block_info.block_number); 
     let mut cached_state = CachedState::new(reader, GlobalContractCache::new(1));
     let chain_id_str = unsafe { CStr::from_ptr(chain_id) }.to_str().unwrap();
@@ -26,11 +46,53 @@ pub extern "C" fn snosRunnerRun(
     // SNOS runner
     // Todo: override input path and block context
     let runner = SnOsRunner::default();
-    
+    let runner = runner.with_input_path("new/input/path"); // Todo: pass in the actual json data
+    let runner = runner.with_block_context(block_context);
 
     // Execute the run method
     match runner.run(shared_state, execution_infos) {
         Ok(result_pie) => println!("Success: {:?}", result_pie),
         Err(e) => eprintln!("Error: {:?}", e),
     }
+}
+
+
+fn build_block_context(
+    state: &mut dyn State,
+    block_info: &BlockInfo,
+    chain_id_str: &str,
+) -> BlockContext {
+    let sequencer_addr =  StarkFelt::new(block_info.sequencer_address).unwrap();
+    let gas_price_wei_felt = StarkFelt::new(block_info.gas_price_wei).unwrap();
+    let gas_price_fri_felt = StarkFelt::new(block_info.gas_price_fri).unwrap();
+    let data_gas_price_wei_felt = StarkFelt::new(block_info.data_gas_price_wei).unwrap();
+    let data_gas_price_fri_felt = StarkFelt::new(block_info.data_gas_price_fri).unwrap();
+    let default_gas_price = NonZeroU128::new(1).unwrap();
+
+    let mut old_block_number_and_hash: Option<BlockNumberHashPair> = None;
+    if block_info.block_number >= 10 {
+        old_block_number_and_hash = Some(BlockNumberHashPair{
+            number: starknet_api::block::BlockNumber(block_info.block_number - 10),
+            hash: BlockHash(StarkFelt::new(block_info.block_hash_to_be_revealed).unwrap()),
+        })
+    }
+    pre_process_block(state, old_block_number_and_hash, BlockifierBlockInfo{
+        block_number: starknet_api::block::BlockNumber(block_info.block_number),
+        block_timestamp: starknet_api::block::BlockTimestamp(block_info.block_timestamp),
+        sequencer_address: ContractAddress(PatriciaKey::try_from(sequencer_addr).unwrap()),
+        gas_prices: GasPrices {
+            eth_l1_gas_price: NonZeroU128::new(felt_to_u128(gas_price_wei_felt)).unwrap_or(default_gas_price),
+            strk_l1_gas_price: NonZeroU128::new(felt_to_u128(gas_price_fri_felt)).unwrap_or(default_gas_price),
+            eth_l1_data_gas_price: NonZeroU128::new(felt_to_u128(data_gas_price_wei_felt)).unwrap_or(default_gas_price),
+            strk_l1_data_gas_price: NonZeroU128::new(felt_to_u128(data_gas_price_fri_felt)).unwrap_or(default_gas_price),
+        },
+        use_kzg_da: block_info.use_blob_data == 1,
+    }, ChainInfo{
+        chain_id: ChainId(chain_id_str.to_string()),
+        fee_token_addresses: FeeTokenAddresses {
+            // both addresses are the same for all networks
+            eth_fee_token_address: ContractAddress::try_from(StarkHash::try_from("0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7").unwrap()).unwrap(),
+            strk_fee_token_address: ContractAddress::try_from(StarkHash::try_from("0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d").unwrap()).unwrap(),
+        },
+    }, get_versioned_constants(block_info.version)).unwrap()
 }
